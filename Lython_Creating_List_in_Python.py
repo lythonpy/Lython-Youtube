a618363c85ebf4d959eabbb40d6fe0da057f5c24
lythonpy <71136630+lythonpy@users.noreply.github.com>
2020-10-07 12:15:06 +0530

    Update Lython_Creating_List_in_Python.py

--- Lython_Creating_List_in_Python.py
+++ Lython_Creating_List_in_Python.py
@@ -1,21 +1,31 @@
 #Creating a Empty List
 a=[]
 print(a)	#prints -> []		
+
 a1=list()	
 print(a1)	#prints -> []
+
 
 #Creating a List of Integers
 b=[1,2,3,4,78,87,90]	
 print(b)	#prints -> [1,2,3,4,78,87,90]
+
+
 #Creating a List of Floats or Floating type Numbers
 c=[1.3,3.32,6.2,6.0,1.36] 
 print(c)	#prints -> [1.3,3.32,6.2,6.0,1.36]
+
+
 #Creating a List of String
 d=["this" ,"is" ,"a" ,"string" ,"list"] 	
 print(d)	#prints-> ["this","is","a","string","list"]
+
+
 #Creating a List of Mixed Data type
 e=["string",123,2.3]		
 print(e)	#prints -> ["string",123,2.3] 
+
+
 #Creating a List of Lists
 l=[a,b,c]
 print(l)	#prints ->[[], [1, 2, 3, 4, 78, 87, 90], [1.3, 3.32, 6.2, 6.0, 1.36]]
